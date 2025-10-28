983a6c8fa9fea9cd19ec00e0c26b9d08b90d0b3f
rob587 <robertocamm02@yahoo.com>
2025-10-28 14:45:33 +0100

    first snack

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// funzione dichiarativa
 
+// function somma(a, b) {
+//   return a + b;
+// }
+
+// somma(5, 3);
+
+// console.log(somma(5, 3));
+
+// funzione anonima assegnata a una variabile
+// const somma = function (a, b) {
+//   return a + b;
+// };
+
+// console.log(somma(5, 3));
+
+// arrow function
+// const somma = (a, b) => {
+//   return a + b;
+// };
+
+// console.log(somma(5, 3));
