365b72f2dffa66659e219bc75c1da8fc0d0e4e78
rob587 <robertocamm02@yahoo.com>
2025-10-28 15:14:46 +0100

    quarto snack

--- script.js
+++ script.js
@@ -32,9 +32,22 @@
 
 // SNACK 3
 
-// creata funzione callback chiamata moltiplica
-const moltiplica = (a, b) => a * b;
-// variabile che accetta 2 argomenti e l'argomento generico, eseguo la funzione che è stata passata come parametro
-const eseguiOperazione = (a, b, operatore) => operatore(a, b);
+// // creata funzione callback chiamata moltiplica
+// const moltiplica = (a, b) => a * b;
+// // variabile che accetta 2 argomenti e l'argomento generico, eseguo la funzione che è stata passata come parametro
+// const eseguiOperazione = (a, b, operatore) => operatore(a, b);
 
-console.log(eseguiOperazione(5, 3, moltiplica));
+// console.log(eseguiOperazione(5, 3, moltiplica));
+
+// SNACK 4
+
+// function creaTimer(time) {
+//   return () => {
+//     setTimeout(() => {
+//       console.log("Tempo scaduto");
+//     }, time);
+//   };
+// }
+
+// const timer = creaTimer(2000);
+// timer();
